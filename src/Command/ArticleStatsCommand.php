346d3b5af5b65b6de0790eeecc8034fe491e1586
Marcelo Almeida <contato@marceloalmeida.dev>
2020-10-31 15:53:12 -0300

    create a command

---
+++ src/Command/ArticleStatsCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ArticleStatsCommand extends Command
+{
+    protected static $defaultName = 'article:stats';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Returns some article stats')
+            ->addArgument('slug', InputArgument::REQUIRED, 'The article\'s slug')
+            ->addOption('format', null, InputOption::VALUE_REQUIRED, 'The output format', 'text')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $slug = $input->getArgument('slug');
+
+        $data = [
+            'slug' => $slug,
+            'hearts' => random_int(10, 100)
+        ];
+
+        if ($slug) {
+            $io->note(sprintf('You passed an argument: %s', $slug));
+        }
+
+        switch ($input->getOption('format')) {
+            case 'text':
+                $rows = [];
+                foreach ($data as $key => $value) {
+                    $rows[] = [$key, $value];
+                }
+                $io->table(['Key', 'Value'], $rows);
+                break;
+            case 'json':
+                $io->write(json_encode($data));
+                break;
+            default:
+                throw new \Exception('What kind of crazy format is that?!');
+        }
+
+        return Command::SUCCESS;
+    }
+}
